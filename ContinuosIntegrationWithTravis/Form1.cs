829d69303ab80078f704855bbb206a2d9311d1ca
sourabhmishra <smishra@allianceglobalservices.com>
2016-01-12 18:45:08 +0530

    Random number generator to be deployed on our travis.

---
+++ ContinuosIntegrationWithTravis/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContinuosIntegrationWithTravis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getrandom = new Random();
+            label1.Text = GetRandomNumber(10, 1000, getrandom).ToString();
+        }
+
+        public static int GetRandomNumber(int min, int max, Random getrandom)
+        {
+            lock (syncLock)
+            { // synchronize
+                return getrandom.Next(min, max);
+            }
+        }
+
+        private Random getrandom;
+        private static readonly object syncLock = new object();
+        
+    }
+}
